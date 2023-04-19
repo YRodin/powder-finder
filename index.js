9183a669ef729f4a35aefa1ceb9d27687801a77a
YRodin <rodin4doc@gmail.com>
2023-04-19 15:01:08 -0400

    update server/index.js

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ const keys = require('./config/keys');
 const passport = require('passport');
 const path = require('path');
 const port = process.env.PORT || 5001;
-// Import and execute passport.js configuration
-require ('./services/passport');
-// Set up Fixie proxy to be used in prod for static IP for Heroku app
+
+require('./services/passport');
+
 const { URL } = require('url');
 const Agent = require('socks5-http-client/lib/Agent');
 const fixieSocksUrl = process.env.FIXIE_SOCKS_URL;
+
 const mongooseOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+
 if (process.env.NODE_ENV === 'production') {
   mongooseOptions.server = {
     socketOptions: {
@@ -32,20 +34,22 @@ if (process.env.NODE_ENV === 'production') {
   };
 }
 
-// Connect DB
 mongoose.connect(keys.MONGODB_URI, mongooseOptions);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, 'build')));
+
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
- }
+}
+
 router(app);
+
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port);
