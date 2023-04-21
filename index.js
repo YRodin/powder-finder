ceadb9882e069693c875d52e0c128ea56ed7a8a5
YRodin <rodin4doc@gmail.com>
2023-04-20 20:43:25 -0400

    modify index.js to ensure that api endpoints get served if accessed

--- index.js
+++ index.js
@@ -10,24 +10,31 @@ const passport = require('passport');
 const path = require('path');
 const port = process.env.PORT || 5001;
 // Import and execute passport.js configuration
-require ('./services/passport');
+require('./services/passport');
 // Connect DB
 mongoose.connect(keys.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, 'build')));
+
+// Apply API routes
+router(app);
+// Serve static files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
+  // Catch-all route for client-side routing in React app
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
- }
-router(app);
+} else {
+  app.use(express.static(path.join(__dirname, 'build')));
+}
+
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port);
