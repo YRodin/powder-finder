724db687522f350b43f6acc55feedf857e096e26
YRodin <rodin4doc@gmail.com>
2023-04-28 16:26:18 -0400

    work on mobile friendly styling of components, add click listener to navbar links and toggle element

--- client/src/features/navbar/Navbar.js
+++ client/src/features/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -13,36 +13,77 @@ const NavBar = () => {
   const { isLoggedIn, userName } = useSelector((state) => state.user);
   const location = useLocation();
   const [showLoginDropdown, setShowLoginDropdown] = useState(false);
+
   useEffect(() => {
     setShowLoginDropdown(location.pathname === "/user/login");
   }, [location.pathname]);
 
+  // control visibility of expanded navbar.collapse element and make it dissapear on user click elsewhere on screen
+  const navBarRef = useRef(null);
+  const [navExpanded, setNavExpanded] = useState(false);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navBarRef.current && !navBarRef.current.contains(event.target)) {
+        setNavExpanded(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <Navbar variant="dark" expand="md" fixed="top" className={styles.navbar}>
+    <Navbar
+      variant="dark"
+      expand="md"
+      fixed="top"
+      className={styles.navbar}
+      ref={navBarRef}
+      expanded={navExpanded}
+      onToggle={() => setNavExpanded(!navExpanded)}
+    >
       <Container className={styles.navContainer}>
         <Navbar.Brand as={Link} to="/">
           <img className={styles.logo} src={logo} alt="PF" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className={styles.navItems}>
-          <Nav>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={`${styles.navItems} ${styles.slidingNavContainer}`}
+        >
+          <Nav className={styles.centeredNavItems}>
             <Nav.Item>
               {!isLoggedIn && (
-                <Nav.Link as={Link} to="/user/signup">
+                <Nav.Link
+                  as={Link}
+                  to="/user/signup"
+                  className={styles.slidingNavLink}
+                  onClick={() => setNavExpanded(false)}
+                >
                   Sign Up
                 </Nav.Link>
               )}
             </Nav.Item>
             <Nav.Item>
               {!isLoggedIn && (
-                <Nav.Link as={Link} to="/user/login">
+                <Nav.Link
+                  as={Link}
+                  to="/user/login"
+                  className={styles.slidingNavLink}
+                  onClick={() => setNavExpanded(false)}
+                >
                   Sign In
                 </Nav.Link>
               )}
             </Nav.Item>
             <Nav.Item>
               {isLoggedIn && (
-                <Nav.Link as={Link} to="/user/edit">
+                <Nav.Link
+                  as={Link}
+                  to="/user/edit"
+                  className={styles.slidingNavLink}
+                  onClick={() => setNavExpanded(false)}
+                >
                   User Settings
                 </Nav.Link>
               )}
@@ -50,9 +91,13 @@ const NavBar = () => {
             <Nav.Item>
               {isLoggedIn && (
                 <Nav.Link
-                  onClick={() => dispatch(signOut())}
+                  onClick={() => {
+                    dispatch(signOut());
+                    setNavExpanded(false);
+                  }}
                   as={Link}
                   to="/user"
+                  className={styles.slidingNavLink}
                 >
                   Sign Out
                 </Nav.Link>
